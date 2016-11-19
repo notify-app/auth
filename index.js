06df2f338c813e9c2b18705627c8fc56f355ddc0
Luca Tabone <taboneluca3@gmail.com>
2016-11-19 16:04:06 +0100

    Implement cluster functionality

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 'use strict'
 
-require('./src/server')
+const cluster = require('cluster')
+const config = require('./config')
+
+if (cluster.isMaster) {
+  processArgs()
+  console.info('deploying auth servers')
+  for (let i = 0; i < config.instances; i ++) {
+    const worker = cluster.fork()
+  }
+} else {
+  console.log(`auth server deployed. PID: '${process.pid}'`)
+  require('./src/server')
+}
+
+function processArgs () {
+  const argv = require('minimist')(process.argv.slice(2))
+
+  if ('dbURL' in argv) config.db.url = argv.dbURL
+  if ('sessionName' in argv) config.session.name = argv.sessionName
+  if ('sessionMaxAge' in argv) config.session.maxAge = argv.sessionMaxAge
+  if ('port' in argv) config.port = argv.port
+  if ('instances' in argv) config.instances = argv.instances
+}
